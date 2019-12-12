945e4625d820dcd7d2b6989c3e97bf850c0f76ab
ExtraALIEN <ExtraAlien@yandex.ru>
2019-12-12 17:19:58 +0300

    Add nginx+gunicorn base

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
+sudo unlink /etc/nginx/sites-enabled/nginx.conf
 sudo ln -s /home/extraalien/projects/extratetris/etc/nginx.conf /etc/nginx/sites-enabled
-sudo nginx -s reload
+sudo service nginx stop
+sudo service nginx start
+killall gunicorn
+gunicorn -c etc/gconf.py testapp:app
